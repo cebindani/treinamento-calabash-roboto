250ae93d465ffaa6dbd8e1044bf8abda42be6928
dmaila <dmaila@ciandt.com>
2016-10-21 00:50:14 -0200

    Implementando steps

--- specs/features/step_definitions/roboto_steps.rb
+++ specs/features/step_definitions/roboto_steps.rb
@@ -1,40 +1,51 @@
 ######### DADO #########
 Given(/^que estou na tela de login$/) do
-  pending
+  @page = page(LoginScreen).await(timeout: 5)
 end
 
 Given(/^preencho o campo "([^"]*)" com "([^"]*)"$/) do |field, text|
-  pending
+  @page.set_field(field, text)
 end
 
 Given(/^que realizei o login com o usuário "([^"]*)" e senha "([^"]*)"$/) do |user, password|
-  pending
+
+  user_field = "User"
+  password_field = "Senha"
+
+  steps %(
+    E que estou na tela de login
+    E preencho o campo "#{user_field}" com "#{user}"
+    E preencho o campo "#{password_field}" com "#{password}"
+    E tocar no botão de login
+    )
+
 end
 
 Given(/^que estou na Home$/) do
-  pending
+  @page = page(HomeScreen).await(timeout: 5)
 end
 
 ######### QUANDO #########
 When(/^tocar no botão de login$/) do
-  pending
+  @page.touch_login_button
 end
 
-When(/^tocar no botão Sair$/) do |button|
-  pending
+When(/^tocar no botão Sair$/) do
+  @page.touch_logoff_button
 end
 
 
 ######### ENTãO #########
-
 Then(/^devo ver a tela de "([^"]*)"$/) do |screen|
-  pending
+  if screen == "Home"
+    @page = page(HomeScreen).await(timeout: 5)
+  end
 end
 
 Then(/^devo ver a mensagem de boas vindas "([^"]*)"$/) do |message|
-  pending
+  @page.has_message? message
 end
 
 Then(/^devo ver a tela de login$/) do
-  pending
+  @page = page(LoginScreen).await(timeout: 5)
 end
