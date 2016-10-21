a155ae6bdc8005499ac6f0cf3058eff6c7d84d5e
dmaila <dmaila@ciandt.com>
2016-10-21 00:47:11 -0200

    Gerando steps

--- specs/features/roboto.feature
+++ specs/features/roboto.feature
@@ -18,5 +18,5 @@ Funcionalidade: Treinamento Calabash Roboto
   Cenário: Ao fazer o logoff, o app deverá redirecionar para a tela de login
     Dado que realizei o login com o usuário "Roboto" e senha "Android"
     E que estou na Home
-    Quando tocar no botão "Sair"
+    Quando tocar no botão Sair
     Então devo ver a tela de login
--- specs/features/step_definitions/roboto_steps.rb
+++ specs/features/step_definitions/roboto_steps.rb
@@ -1,5 +1,40 @@
 ######### DADO #########
+Given(/^que estou na tela de login$/) do
+  pending
+end
+
+Given(/^preencho o campo "([^"]*)" com "([^"]*)"$/) do |field, text|
+  pending
+end
+
+Given(/^que realizei o login com o usuário "([^"]*)" e senha "([^"]*)"$/) do |user, password|
+  pending
+end
+
+Given(/^que estou na Home$/) do
+  pending
+end
 
 ######### QUANDO #########
+When(/^tocar no botão de login$/) do
+  pending
+end
+
+When(/^tocar no botão Sair$/) do |button|
+  pending
+end
+
 
 ######### ENTãO #########
+
+Then(/^devo ver a tela de "([^"]*)"$/) do |screen|
+  pending
+end
+
+Then(/^devo ver a mensagem de boas vindas "([^"]*)"$/) do |message|
+  pending
+end
+
+Then(/^devo ver a tela de login$/) do
+  pending
+end
